1a1c3bb0f47aefd7c4d555217de5c700e7c37a70
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-10-04 15:53:09 -0600

    added functions in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const openDB = await openDB("jate", 1 );
+  const tx = openDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: 1, cont: content });
+  const result = await request;
+  console.log("data saved to the database", result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const openDB = await openDB("jate", 1);
+  const tx = openDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("data retrieved", result);
+  return result;
+}
 
 initdb();

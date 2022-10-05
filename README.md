90a65f01d9428035d7495f7ca2862a281c8fcc04
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-10-05 16:00:03 -0600

    continued to debug app, wrote markdown

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Text_Editor_App
+##JATE, Just Another Text Editor
+
+## Description
+
+This application allows users to create notes or code snippets regardless of having an internet connection. This application runs in the browser as well as having a progressive web app installation feature and can function on or offline.
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Credits](#usage)
+4. [Licensing](#licensing)
+5. [Tests](#test)
+6. [Questions](#questions)
+
+## Installation
+
+The GitHub Repository can be found here: https://github.com/mbottoms3/Text_Editor_App
+
+## Usage
+
+NA
+
+## Credits
+
+There were no collaborators on this project
+
+## Licensing
+
+This project uses the MIT license.
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://choosealicense.com/licenses/mit/)
+
+## Tests
+
+NA
+
+## Questions
+
+If there are any questions please reach out to me at bottoms.micah.3@gmail.com
+
+Github profile: [mbottoms3](https://github.com/mbottoms3)
+
+![Screenshot of app]("Screenshot of App")
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,35 +1,34 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-  export const putDb = async (content) => {
-    const openDB = await openDB("jate", 1 );
-    const tx = openDB.transaction("jate", "readwrite");
-    const store = tx.objectStore("jate");
-    const request = store.put({id: 1, stuff: content });
-    const result = await request;
-    console.log("data saved to the database", result);
-  }
-  
-  export const getDb = async () => {
-    const openDB = await openDB("jate", 1);
-    const tx = openDB.transaction("jate", "readonly");
-    const store = tx.objectStore("jate");
-    const request = store.getAll();
-    const result = await request;
-    console.log("data retrieved", result);
-    return result;
-  }
+export const putDb = async (content) => {
+  const openDB = await openDB("jate", 1);
+  const tx = openDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, stuff: content });
+  const result = await request;
+  console.log("data saved to the database", result);
+};
 
-  
+export const getDb = async () => {
+  const openDB = await openDB("jate", 1);
+  const tx = openDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("data retrieved", result);
+  return result;
+};
+
 initdb();

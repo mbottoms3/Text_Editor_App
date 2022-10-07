83cd7f59de58c3bad9cb1cedd333e010c3781429
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-10-07 14:49:54 -0600

    debug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const { LibManifestPlugin } = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const { LibManifestPlugin } = require("webpack");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: "./index.html",
         title: "Jate",
       }),
 
       new InjectManifest({
-        swSrc:  './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
 
       new WebpackPwaManifest({
-        filename: 'manifest.json',
-        display: 'standalone',
-        orientation: 'portrait',
+        filename: "manifest.json",
+        display: "standalone",
+        orientation: "portrait",
         fingerprints: false,
         inject: true,
         name: "Progressive Web Application",
         short_name: "jate",
         description: "PWA Text Editor",
-        background_color: '#ffffff',
-        start_url: '/',
-        publicPath: '/',
+        background_color: "#ffffff",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
@@ -53,20 +53,19 @@ module.exports = () => {
         // CSS Loaders
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         // Babel
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
             },
           },
         },
-        
       ],
     },
   };
